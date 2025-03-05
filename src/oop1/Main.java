396178bbfba2a5a837c92f89ec99acc25d7b7d50
Azizbek <muhammadnazarovazizbek@gmail.com>
2025-03-05 16:18:43 +0500

    first commit

---
+++ src/oop1/Main.java
@@ -0,0 +1,21 @@
+package oop1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle rect1 = new Rectangle(4, 40);
+        Rectangle rect2 = new Rectangle(3.5, 35.9);
+
+        System.out.println("Rectangle 1:");
+        System.out.println("Kengligi: " + rect1.width);
+        System.out.println("Balandligi: " + rect1.height);
+        System.out.println("Yuzi: " + rect1.getArea());
+        System.out.println("Perimeteri: " + rect1.getPerimeter());
+
+        System.out.println("\nRectangle 2:");
+        System.out.println("Kengligi: " + rect2.width);
+        System.out.println("Balandligi: " + rect2.height);
+        System.out.println("Yuzi: " + rect2.getArea());
+        System.out.println("Perimeteri: " + rect2.getPerimeter());
+    }
+}
