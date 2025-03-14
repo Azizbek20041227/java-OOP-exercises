f1ea9733b7ba67fc7e4a7cc11c45fc6e3d6e5cee
Azizbek <muhammadnazarovazizbek@gmail.com>
2025-03-14 07:13:20 +0500

    first commit

---
+++ src/exercises4_3/TestMain.java
@@ -0,0 +1,12 @@
+package exercises4_3;
+
+public class TestMain {
+    public static void main(String[] args) {
+
+        Point2D p2d = new Point2D(3, 4);
+        System.out.println("Point2D: " + p2d);
+
+        Point3D p3d = new Point3D(5, 6, 7);
+        System.out.println("Point3D: " + p3d);
+    }
+}
